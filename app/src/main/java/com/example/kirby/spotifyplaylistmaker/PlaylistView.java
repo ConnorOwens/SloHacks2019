ba1b2f53d933eb642a38a33d588b21068ca83fdb
Connor Owens <owensconnor1@gmail.com>
2019-02-02 11:24:20 -0800

    Merge branch 'master' of https://github.com/ConnorOwens/SloHacks2019
    # Conflicts:
    #	app/src/main/java/com/example/kirby/spotifyplaylistmaker/AddToPlaylist.java

--- app/src/main/java/com/example/kirby/spotifyplaylistmaker/PlaylistView.java
+++ app/src/main/java/com/example/kirby/spotifyplaylistmaker/PlaylistView.java
@@ -3,10 +3,15 @@ package com.example.kirby.spotifyplaylistmaker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
 public class PlaylistView extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,19 @@ public class PlaylistView extends AppCompatActivity {
                 startActivity(new Intent(PlaylistView.this, AddToPlaylist.class));
             }
         });
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        String[] myDataset = {"peepee", "poopoo"};
+        // specify an adapter (see also next example)
+        mAdapter = new PlaylistViewAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 }
