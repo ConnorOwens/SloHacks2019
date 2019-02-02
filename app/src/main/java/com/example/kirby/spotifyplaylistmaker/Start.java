8cf9f9b150cfb3cd2fb685acc5d462dd5e7eb748
Connor Owens <owensconnor1@gmail.com>
2019-02-02 00:34:24 -0800

    Created branches for activites

--- app/src/main/java/com/example/kirby/spotifyplaylistmaker/Start.java
+++ app/src/main/java/com/example/kirby/spotifyplaylistmaker/Start.java
@@ -1,7 +1,10 @@
 package com.example.kirby.spotifyplaylistmaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Start extends AppCompatActivity {
 
@@ -9,6 +12,23 @@ public class Start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
+        final Button newPlaylist = findViewById(R.id.button4);
+        newPlaylist.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Start.this, NameNewPlaylist.class));
+            }
+        });
+        final Button editAnOldPlaylist = findViewById(R.id.button5);
+        editAnOldPlaylist.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Start.this, FindAPlaylist.class));
+            }
+        });
+        final Button addToAPlaylist = findViewById(R.id.button3);
+        addToAPlaylist.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Start.this, AddToPlaylist.class));
+            }
+        });
     }
 }
