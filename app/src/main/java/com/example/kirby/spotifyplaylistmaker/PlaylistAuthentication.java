d75593a6e289a2c9a5ee91c912389cbb473f1e16
Connor Owens <owensconnor1@gmail.com>
2019-02-02 18:03:36 -0800

    Merge branch 'master' of https://github.com/ConnorOwens/SloHacks2019
    # Conflicts:
    #	app/src/main/java/com/example/kirby/spotifyplaylistmaker/AddToPlaylist.java

--- app/src/main/java/com/example/kirby/spotifyplaylistmaker/PlaylistAuthentication.java
+++ app/src/main/java/com/example/kirby/spotifyplaylistmaker/PlaylistAuthentication.java
@@ -1,7 +1,10 @@
 package com.example.kirby.spotifyplaylistmaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlaylistAuthentication extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class PlaylistAuthentication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist_authentication);
+        final Button searchPlaylist = findViewById(R.id.searchPlaylistButton);
+        searchPlaylist.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //authenticate
+            }
+        });
     }
 }
