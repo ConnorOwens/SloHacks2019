1998c207d37fcc96dce8324d3c49bf8aa1bef434
Colem18 <cole2018@gmail.com>
2019-02-02 16:24:10 -0800

    commit sudoku

--- app/src/main/java/com/example/kirby/spotifyplaylistmaker/AddToPlaylist.java
+++ app/src/main/java/com/example/kirby/spotifyplaylistmaker/AddToPlaylist.java
@@ -1,27 +1,33 @@
 package com.example.kirby.spotifyplaylistmaker;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AddToPlaylist extends AppCompatActivity {
+import java.util.List;
 
+public class AddToPlaylist extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_playlist);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference playListReference = database.getReference("playlist");
-
-
     }
-
+    public void addSong(View view) {
+        EditText songEditText = (EditText) findViewById(R.id.songName);
+        EditText artistEditText = (EditText) findViewById(R.id.songArtist);
+        String newSongName =  songEditText.getText().toString();
+        String newSongArtist = artistEditText.getText().toString();
+        List songList = databaseReference.child("playlistNames").child().child("songList");
+        Song newSong = new Song();
+        newSong.setSongName(newSongName);
+        newSong.setArtist(newSongArtist);
+        songList.add(newSong);
+        databaseReference.child("plalistNames").child().child("songList").setValue(songList);
+    }
 }
